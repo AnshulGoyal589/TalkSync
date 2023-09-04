f6056728819c6f6db4a6c10bd0808cc56e97cde1
Anshul Goyal <anshulgoyal589@gmail.com>
2023-09-05 01:29:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,16 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 const path=require("path");
+const colors=require("colors");
+const PORT=8000;
 
 
 app.use("/", express.static(path.join(__dirname, "public")));
 
 const users={};
+const chatWith={};
+const chatWith2={};
+let chatWith3;
 
 
 app.get('/', (req, res) => {
@@ -18,26 +23,60 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
+
+
+   
     socket.on('chat message', (msg) => {
-      io.emit('chat message', {msg: msg,   username : users[socket.id]});
+      
+      socket.emit('chat message', {msg: msg,   username : users[socket.id]});
+      socket.to(chatWith3).emit('chat message', {msg: msg,   username : users[socket.id]});
+
     });
+    socket.on("abc",(data)=>{
+      chatWith3=chatWith2[data];
+      // socket.join("room");
+
+
+        // const socket1Id = socket.id;
+        // const socket2Id = chatWith2[data];
+        // const roomName = 'room';
+
+        // socket.joinToRoom = function(socketId, roomName) {
+        // const socket2 = io.sockets.connected[socketId];
+
+        // if (socket2) {
+        //   socket2.join(roomName);
+        //   console.log(`Socket with ID ${socketId} joined room ${roomName}`);
+        // } else {
+        //   console.log(`Socket with ID ${socketId} not found`);
+        // }
+        // };
+        // socket.joinToRoom(socket2Id, roomName);
+
+
+    })
     socket.on('login', (username) => {
       users[socket.id]=username;
+      chatWith[socket.id]=username;
+      chatWith2[username]=socket.id;
+      io.emit('login2',chatWith);
     });
     socket.on('someoneTyping', (info) => {
       if(!users[socket.id]){
         users[socket.id]="Anonymous";
       }
-      socket.broadcast.emit('someoneTyping',users[socket.id]+" "+info)
+      // socket.broadcast.emit('someoneTyping',users[socket.id]+" "+info);
+      socket.to(chatWith3).emit('someoneTyping',users[socket.id]+" "+info);
     });
     socket.on('someoneNotTyping', (info) => {
       if(!users[socket.id]){
         users[socket.id]="Anonymous";
       }
-      socket.broadcast.emit('someoneNotTyping',users[socket.id]+" "+info)
+      // socket.broadcast.emit('someoneNotTyping',users[socket.id]+" "+info);
+      socket.to(chatWith3).emit('someoneNotTyping',users[socket.id]+" "+info);
     });
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000'); 
+server.listen(PORT, () => {
+  console.log("Server is listening at PORT ".blue , `http://localhost:${PORT}`.red); 
 });
