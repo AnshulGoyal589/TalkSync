43a5aa5d02d77263965e210d313617da9d250571
Anshul Goyal <anshulgoyal589@gmail.com>
2023-09-05 01:29:27 +0530

    Update client.js

--- public/client.js
+++ public/client.js
@@ -4,22 +4,49 @@ var messages = document.getElementById('messages');
 var form = document.getElementById('form');
 var input = document.getElementById('input');
 var typing = document.getElementById('typing');
+var messagesHolder = document.getElementById('messagesHolder');
 var login = document.getElementById('login');
 var loginForm = document.getElementById('loginForm');
 var username = document.getElementById('username');
-var messagesHolder = document.getElementById('messagesHolder');
+var hero2 = document.getElementById('hero2');
+// var spaces = document.querySelectorAll('.space');
+
+
+
+
+
 
 
 
 loginForm.addEventListener("submit",(e)=>{
   e.preventDefault();
+  socket.on('login2',(chatWith)=>{
+    // console.log(chatWith);  
+    while (hero2.firstChild) {
+      hero2.removeChild(hero2.firstChild);
+    }
+    for(const chatter in chatWith){
+      const person=document.createElement('div');
+      person.innerText=chatWith[chatter];
+      person.style.width="100%";
+      person.style.className="space";
+      person.style.height="80px"; 
+      person.style.color="white";
+      person.style.border="2px solid white";
+      hero2.appendChild(person); 
+
+    }
+  })
+
   if(username.value){
     socket.emit('login',username.value);
     username.value='';
   }else{
-    socket.emit('login',"Anonymous");
-  }
+    socket.emit('login',"Anonymous");  
+  } 
 })
+
+
 form.addEventListener('submit', function(e) {
   e.preventDefault();
   if (input.value) {
@@ -27,6 +54,13 @@ form.addEventListener('submit', function(e) {
     input.value = '';
   }
 });
+
+hero2.addEventListener("click",(e)=>{
+  // console.log(e.target);
+  socket.emit("abc",e.target.innerText);
+})
+
+
 
 
 socket.on('chat message', (data)=> {
@@ -37,8 +71,6 @@ socket.on('chat message', (data)=> {
   messagesHolder.scrollTop = messagesHolder.scrollHeight;
   
 });
-
-
 
 input.addEventListener("keydown",(e)=>{ 
   socket.emit("someoneTyping","is typing...")
@@ -51,10 +83,7 @@ socket.on('someoneTyping',(info)=>{
 
 
 input.addEventListener("keyup",(e)=>{ 
-  socket.emit("someoneNotTyping","is typing...")
-  // socket.on('someoneTyping',(info)=>{
-    // typing.innerText="";                      // writing this line inside will ig nly clear inner text of cuurent user, not all
-  // })
+  socket.emit("someoneNotTyping","is typing...");
 })
 socket.on('someoneNotTyping',(info)=>{
   setTimeout(function() {
